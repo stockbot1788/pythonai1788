2bbc63f35cb48499377af5c176a9245234a153e4
anson <cppany@cathaypacific.com>
2017-12-04 20:05:50 +0800

    update gpu

--- aitensorflow.py
+++ aitensorflow.py
@@ -26,73 +26,74 @@ from tensorflow.python.client import device_lib
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 print(device_lib.list_local_devices())
 
-ls1Ip = Input(shape=(30,5))
-ls11 = LSTM(25,dropout=0.5, recurrent_dropout=0.3, activation='sigmoid')(ls1Ip)
-ls12 = Dense(10, activation='relu')(ls11)
-ls12d = Dropout(.3)(ls12)
-ls13 = Dense(5, activation='tanh')(ls12d)
+with tf.device('/gpu:1'):
+    ls1Ip = Input(shape=(30,5))
+    ls11 = LSTM(25,dropout=0.5, recurrent_dropout=0.3, activation='sigmoid')(ls1Ip)
+    ls12 = Dense(10, activation='relu')(ls11)
+    ls12d = Dropout(.3)(ls12)
+    ls13 = Dense(5, activation='tanh')(ls12d)
 
-ls1IpR = Input(shape=(2,))
-ls1IpR2 = Dense(5, activation='tanh')(ls1IpR)
+    ls1IpR = Input(shape=(2,))
+    ls1IpR2 = Dense(5, activation='tanh')(ls1IpR)
 
-M1 = concatenate([ls13, ls1IpR2])
-
-
-ls2Ip2 = Input(shape=(30,4))
-ls21 = LSTM(25,dropout=0.5, recurrent_dropout=0.3,activation='sigmoid')(ls2Ip2)
-ls22 = Dense(20, activation='relu')(ls21)
-ls22d = Dropout(.3)(ls22)
-ls23 = Dense(10, activation='tanh')(ls22d)
-ls24 = Dense(5, activation='tanh')(ls23)
-
-ls2IpR = Input(shape=(2,))
-ls2IpR2 = Dense(5, activation='tanh')(ls2IpR)
-
-M2 = concatenate([ls24, ls2IpR2])
-
-ls3Ip3 = Input(shape=(1,))
-ls31 = Dense(5, activation='tanh')(ls3Ip3)
-
-merge_one = concatenate([M1, M2, ls31])
-
-output = Dense(20, activation='tanh')(merge_one)
-output1 = Dense(5, activation='tanh')(output)
-output2 = Dense(1, activation='sigmoid')(output1)
-model = Model(inputs=[ls1Ip,ls1IpR,ls2Ip2,ls2IpR,ls3Ip3], outputs=output2)
-
-#model = Model(inputs=[ls1],outputs=)
-sgd = optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0)
-model.compile(loss='binary_crossentropy',  optimizer=sgd ,metrics=['accuracy'])
-print(model.summary())
-
-print("preparing data")
-PrepareData = DataFormator(env.data)
-inputX,inputXStr,inputX2,inputX2Str,Position,outputY = PrepareData.formatData()
+    M1 = concatenate([ls13, ls1IpR2])
 
 
-inputX = array(inputX)
-inputXStr = array(inputXStr)
-inputX2 = array(inputX2)
-inputX2Str = array(inputX2Str)
-Position = array(Position)
-outputY = array(outputY)
+    ls2Ip2 = Input(shape=(30,4))
+    ls21 = LSTM(25,dropout=0.5, recurrent_dropout=0.3,activation='sigmoid')(ls2Ip2)
+    ls22 = Dense(20, activation='relu')(ls21)
+    ls22d = Dropout(.3)(ls22)
+    ls23 = Dense(10, activation='tanh')(ls22d)
+    ls24 = Dense(5, activation='tanh')(ls23)
 
-# _path = "lstm.h5"
-# if _path and path.isfile(_path):
-#     try:
-#         print("try load weight")
-#         model.load_weights(_path)
-#         print("load weight success")
-#     except Exception as ex:
-#         print("error",ex)
-#         sys.exit("Error message")
+    ls2IpR = Input(shape=(2,))
+    ls2IpR2 = Dense(5, activation='tanh')(ls2IpR)
 
-#print(inputX.shape)
-# for step in range(30):
-#     cost = model.train_on_batch([inputX,inputXStr,inputX2,inputX2Str,Position], outputY) 
-#     print (cost)
-model.fit([inputX,inputXStr,inputX2,inputX2Str,Position], outputY, batch_size=300, epochs=800, verbose=1)
-model.save_weights("lstm2.h5")
+    M2 = concatenate([ls24, ls2IpR2])
+
+    ls3Ip3 = Input(shape=(1,))
+    ls31 = Dense(5, activation='tanh')(ls3Ip3)
+
+    merge_one = concatenate([M1, M2, ls31])
+
+    output = Dense(20, activation='tanh')(merge_one)
+    output1 = Dense(5, activation='tanh')(output)
+    output2 = Dense(1, activation='sigmoid')(output1)
+    model = Model(inputs=[ls1Ip,ls1IpR,ls2Ip2,ls2IpR,ls3Ip3], outputs=output2)
+
+    #model = Model(inputs=[ls1],outputs=)
+    sgd = optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0)
+    model.compile(loss='binary_crossentropy',  optimizer=sgd ,metrics=['accuracy'])
+    print(model.summary())
+
+    print("preparing data")
+    PrepareData = DataFormator(env.data)
+    inputX,inputXStr,inputX2,inputX2Str,Position,outputY = PrepareData.formatData()
+
+
+    inputX = array(inputX)
+    inputXStr = array(inputXStr)
+    inputX2 = array(inputX2)
+    inputX2Str = array(inputX2Str)
+    Position = array(Position)
+    outputY = array(outputY)
+
+    # _path = "lstm.h5"
+    # if _path and path.isfile(_path):
+    #     try:
+    #         print("try load weight")
+    #         model.load_weights(_path)
+    #         print("load weight success")
+    #     except Exception as ex:
+    #         print("error",ex)
+    #         sys.exit("Error message")
+
+    #print(inputX.shape)
+    # for step in range(30):
+    #     cost = model.train_on_batch([inputX,inputXStr,inputX2,inputX2Str,Position], outputY) 
+    #     print (cost)
+    model.fit([inputX,inputXStr,inputX2,inputX2Str,Position], outputY, batch_size=300, epochs=800, verbose=1)
+    model.save_weights("lstm2.h5")
 
 # for i in range(200):
 #     model.fit(np.array(inputX), outputY, batch_size=5000, epochs=100, verbose=1)
