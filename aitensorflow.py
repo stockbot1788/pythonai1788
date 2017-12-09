7d469e8e70d579fc0a6e2b59676353833bfd7277
anson <cppany@cathaypacific.com>
2017-12-10 02:27:59 +0800

    no message

--- aitensorflow.py
+++ aitensorflow.py
@@ -29,12 +29,12 @@ x_drop4 = Dropout(0.5)(ls11)
 ls12 = Conv2D(64, (2, 2), padding='same', activation='relu')(x_drop4)
 ls13 = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(ls12)
 out = Flatten()(ls13)
-output1 = Dense(10, activation='tanh')(out)
-output2 = Dense(5, activation='sigmoid')(output1)
+output1 = Dense(10, activation='softmax')(out)
+output2 = Dense(5, activation='softmax')(output1)
 
 
 ls1IpR = Input(shape=(2,))
-ls1IpR2 = Dense(5, activation='tanh')(ls1IpR)
+ls1IpR2 = Dense(5, activation='softmax')(ls1IpR)
 
 M1 = concatenate([output2, ls1IpR2])
 
@@ -45,24 +45,24 @@ x_drop = Dropout(0.5)(ls21)
 ls22 = Conv2D(64, (2, 2), padding='same', activation='relu')(x_drop)
 ls23 = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(ls22)
 out2 = Flatten()(ls23)
-output21 = Dense(10, activation='tanh')(out2)
+output21 = Dense(10, activation='softmax')(out2)
 x_drop2 = Dropout(0.5)(output21)
-output22 = Dense(5, activation='sigmoid')(x_drop2)
+output22 = Dense(5, activation='softmax')(x_drop2)
 
 ls2IpR = Input(shape=(2,))
-ls2IpR2 = Dense(5, activation='tanh')(ls2IpR)
+ls2IpR2 = Dense(5, activation='softmax')(ls2IpR)
 
 M2 = concatenate([output22, ls2IpR2])
 
 ls3Ip3 = Input(shape=(1,))
-ls31 = Dense(5, activation='tanh')(ls3Ip3)
+ls31 = Dense(5, activation='softmax')(ls3Ip3)
 
 merge_one = concatenate([M1, M2, ls31])
 
-output = Dense(20, activation='tanh')(merge_one)
+output = Dense(20, activation='softmax')(merge_one)
 x_drop3 = Dropout(0.5)(output)
-output1 = Dense(5, activation='tanh')(x_drop3)
-output2 = Dense(1, activation='sigmoid')(output1)
+output1 = Dense(5, activation='softmax')(x_drop3)
+output2 = Dense(1, activation='softmax')(output1)
 model = Model(inputs=[ls1Ip,ls1IpR,ls2Ip,ls2IpR,ls3Ip3], outputs=output2)
 
 sgd = optimizers.RMSprop(lr=0.1, rho=0.9, epsilon=1e-08, decay=0.0)
